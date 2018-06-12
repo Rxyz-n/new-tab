61bca77d0adf73e9564041c01819d3120742c8f2
Nano <hikarishido1997@gmail.com>
2018-06-12 20:38:51 +0700

    finished

---
+++ index.html
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>New Tab</title>
+	<link href="https://fonts.googleapis.com/css?family=Nunito:400,700&amp;subset=vietnamese" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="video-background">
+		<video src="videos/FJ2.mp4" loop autoplay muted></video>
+	</div>
+
+	<div class="clock">
+		<div>
+			<h1 id="time"></h1>
+			<span id="ampm"></span>
+		</div>
+		<h3 id="greetings"></h3>
+	</div>
+	<script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,28 @@
+const NAME  = 'Nano'
+
+const calcTime = () => {
+	let	ampm  = "AM",
+			sal   = "morning",
+			date  = new Date(),
+			hours = date.getHours(),
+			mins  = date.getMinutes()
+
+	hours > 12 && (sal = "afternoon")
+	hours > 18 && (sal = "evening")
+	hours > 11 && hours < 24 && (ampm = "PM")
+	hours > 12 && (hours -= 12)
+	hours < 10 && (hours = '0' + hours)
+	mins  < 10 && (mins = '0' + mins)
+
+	renderClock(hours, mins, sal, ampm)	
+}
+
+const renderClock = (h, m, s, a) => {
+	const clock = document.querySelector('.clock')
+	
+	clock.querySelector('#time').innerText = `${h}:${m}`
+	clock.querySelector('#ampm').innerText = a
+	clock.querySelector('#greetings').innerText = `Good ${s}, ${NAME}`
+}
+
+setInterval(() => calcTime(), 500)
