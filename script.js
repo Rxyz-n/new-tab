c27a723b224c67733606dfcc977eb6b1531f06f2
Nano <hikarishido1997@gmail.com>
2019-04-13 20:23:49 +0700

    handle display either image or video depend on the file name

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-// const NAME  = 'Nano'
+const YOUR_NAME = 'Nano';
+const FILE_NAME = 'background.jpg';
+
+const $ = document.querySelector.bind(document);
+
+(() => {
+  const imgExtRegex = /(.jpg|.png|.jpeg)$/i;
+  const fileType = imgExtRegex.test(FILE_NAME) ? 'image' : 'video';
+
+  if(fileType === 'image') {
+    $('.full-screen').style.background = `url(assets/${FILE_NAME})`;
+    return;
+  }
+
+  const video = document.createElement('video');
+  video.src = `assets/${FILE_NAME}`;
+  video.autoplay = true;
+  video.loop = true;
+  video.muted = true;
+
+  $('.full-screen').appendChild(video);
+})();
 
 // const calcTime = () => {
 // 	let	ampm  = "AM",
@@ -14,12 +35,12 @@
 // 	hours < 10 && (hours = '0' + hours)
 // 	mins  < 10 && (mins = '0' + mins)
 
-// 	renderClock(hours, mins, sal, ampm)	
+// 	renderClock(hours, mins, sal, ampm)
 // }
 
 // const renderClock = (h, m, s, a) => {
 // 	const clock = document.querySelector('.background')
-	
+
 // 	clock.querySelector('#time').innerText = `${h}:${m}`
 // 	clock.querySelector('#ampm').innerText = a
 // 	clock.querySelector('#greetings').innerText = `Good ${s}, ${NAME}`
